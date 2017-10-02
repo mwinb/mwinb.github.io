1c45737487e6707da912f9fcec00bed93cbbd6ff
Michael Winberry <mwinberry0101@gmail.com>
2017-10-02 13:14:02 -0700

    Fixed adjustments to sinNoStroke.js that were not saved.

--- processing/sinNoStroke.js
+++ processing/sinNoStroke.js
@@ -1,27 +1,28 @@
 
 
+var count = 0;
 function setup() {
     createCanvas(1250, 650);
     background(255);
     colorMode(HSB);
-    var counter = 0;
 }
 
 function draw() {
     counter += 1;
     noStroke();
     fill(random(255),100,255,50);
-        fill(random(255),100,255,50);
-        ellipse(width/2, random(650), random(0,200)*cos(counter), random(0,300)*tan(counter));
-        fill(random(255),100,255,50);
+    fill(random(255),100,255,50);
+    ellipse(width/2, random(650), random(0,200)*cos(counter), random(0,300)*tan(counter));
+    fill(random(255),100,255,50);
     
-        ellipse(random(1250), height/2, random(0,200)*cos(counter), random(0,200)*sin(counter));
-        fill(random(255),100,255,50);
+    ellipse(random(1250), height/2, random(0,200)*cos(counter), random(0,200)*sin(counter));
+    fill(random(255),100,255,50);
     ellipse(width/3, height/2, random(0,200)*cos(counter), random(0,200)*tan(counter));
     fill(random(255),100,255,50);
     ellipse(800, height/2, random(0,200)*cos(counter), random(0,200)*tan(counter));
     if(counter>=200) {
-    draw2();
+        draw2();
+    }
     
 };
 
