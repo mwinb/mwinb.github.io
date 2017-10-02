53c37117611cb72c3214143ab75e7a6895501d62
Michael Winberry <mwinberry0101@gmail.com>
2017-10-02 13:27:01 -0700

    Version issues after computer crash, testing processing embedding

--- processing/sinNoStroke.js
+++ processing/sinNoStroke.js
@@ -1,43 +1,39 @@
+var counter = 0;
 
-
-var count = 0;
 function setup() {
     createCanvas(1250, 650);
     background(255);
     colorMode(HSB);
+   
 }
 
 function draw() {
     counter += 1;
-    noStroke();
+    noStroke()
     fill(random(255),100,255,50);
     fill(random(255),100,255,50);
     ellipse(width/2, random(650), random(0,200)*cos(counter), random(0,300)*tan(counter));
+
     fill(random(255),100,255,50);
-    
     ellipse(random(1250), height/2, random(0,200)*cos(counter), random(0,200)*sin(counter));
-    fill(random(255),100,255,50);
-    ellipse(width/3, height/2, random(0,200)*cos(counter), random(0,200)*tan(counter));
-    fill(random(255),100,255,50);
-    ellipse(800, height/2, random(0,200)*cos(counter), random(0,200)*tan(counter));
     if(counter>=200) {
-        draw2();
+    draw2();
     }
     
 };
 
 function draw2() {
     counter += 1
+    noStroke()
     fill(random(255), 100, 255, 50);
-    ellipse(width+(random(-40,-30)),height-=75, 80*cos(counter), random(0,200)*sin(counter));
+    ellipse(1200, 75, 80*cos(counter), random(0,200)*sin(counter));
 
     fill(random(255), 100, 255, 50);
-    ellipse(width-(random(1200,1210)), 75, 80*cos(counter), random(0,200)*sin(counter));
+    ellipse(50, 75, 80*cos(counter), random(0,200)*sin(counter));
 
     fill(random(255), 100, 255, 50);
-    ellipse(width-(random(1200, 1210)), 575 , 80*cos(counter), random(0,200)*sin(counter));
+    ellipse(50, 575 , 80*cos(counter), random(0,200)*sin(counter));
 
     fill(random(255), 100, 255, 50);
-    ellipse(width+(random(-40, -30)), 575, 80*cos(counter), random(0,200)*sin(counter));
-    
+    ellipse(1200, 574, 80*cos(counter), random(0,200)*sin(counter));
 };
