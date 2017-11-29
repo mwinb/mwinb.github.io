466ef17e0a23c00189d4c429eec3c206b9bf1fb2
mwinb <mwinberry0101@gmail.com>
2017-11-29 14:03:30 -0800

    Created slideshow for projects. Test.

---
+++ portfolio.js
@@ -0,0 +1,14 @@
+//Portfolio 
+//Michael Winberry
+
+$(document).ready( function () {
+//project array [[title, date, imageLink, image, about, sourceCodeLink]]
+var projects = [
+['Knil' + "'" + 's' + 'jQuery Arena', 'Spring 17', '/knil', 'Knil' + "'" + 's' + 
+'jQuery arena is a homage to Zelda built using jQuery JavaScript library.' 
++ 'It was my first attempt at programming and allows the '
++ 'user to take control of Knil and fight their way through two' 
++ 'levels in order to find the obtain the treasured "Knil Force".', 
+'https://github.com/mwinb/knil'],
+[]];
+});
