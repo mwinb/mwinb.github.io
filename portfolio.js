5e26910ace5d58946f7a105ac5c0ce2c0c1708f8
mwinb <mwinberry0101@gmail.com>
2017-11-30 01:46:25 -0800

    Complete Design overhaul. Adjusted buttons, title, borders, color scheme, etc ...

--- portfolio.js
+++ portfolio.js
@@ -5,25 +5,22 @@ $(document).ready( function () {
 	var count = 0;
 	//project array [[title, date, image, imageLink, about, sourceCodeLink]]
 	var projects = [
-	['Knil' + "'" + 's' + ' jQuery Arena', 'Spring 2015', 'knilImage.jpeg', '/knil', 
-	'Knil' + "'" + 's' 
-	+ 'jQuery arena is a homage to Zelda built using jQuery JavaScript library.' 
-	+ ' It was my first attempt at programming and allows the '
-	+ 'user to take control of Knil and fight their way through two' 
-	+ 'levels in order to find the obtain the treasured "Knil Force".', 
+	['Knils Arena', 'Spring 2015', 'knilImage.jpeg', '/knil', 
+	'Knils Arena is an adventure game built using jQuery JavaScript library.' 
+	+ ' Knils Arena allows the user to take control of  the Hero Knil and fight their way through two' 
+	+ ' levels in order to find the obtain the treasured "Knil Force".', 
 	'https://github.com/mwinb/knil'],
 	['Apache Company Site', 'Spring 2015', 'apacheco.jpg','/apacheco', 
-	'A website that I created in order to help learn html, css, and js. ' 
-	+ 'Based on the Marine Unit that I deployed to Afghanistan in 2011 with. ', 
+	'A website based on the Marine unit that I deployed to Afghanistan with, in 2011. ', 
 	'https://github.com/mwinb/apacheco'],
 	['Black Jack', 'Spring 2016', 'blackJackImage.jpeg','/blackjack', 
 	'A web based game of Black Jack using the jQuery JavaScript library. ' 
-	+ 'Pits the user against a compure dealer and has 3 modes of difficulty.',
+	+ 'Pits the user against a dealer and has 3 modes of difficulty.',
 	'https://github.com/mwinb/blackjack'],
 	['TerminalDocs/Temdocs', 'Spring 17', 'termdocsImage.jpeg', 'https://github.com/mwinb/termdocs', 
 	'TerminalDocs is a console based text editor that I created using Python 2.7. ' 
 	+ 'I created TerminalDocs in order to help me comment, read, and edit code documents' 
-	+ 'line by line. Supports many commands such as -oe wich opens current file into' 
+	+ ' line by line and without distraction. Supports many commands such as -oe which opens current file into' 
 	+ ' users default text editor. ',
 	'https://github.com/mwinb/TerminalDocs'],
 	['Zombulator', 'Fall 17', 'zombulatorImage.png', '/cs160Zombulator',
